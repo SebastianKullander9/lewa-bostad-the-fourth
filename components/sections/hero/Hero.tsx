c78f49b975bd62ba17831c21a1f673416cf6cbc7
SebastianKullander9 <sebastian.kullander@protonmail.com>
2026-06-05 17:49:17 +0200

    change hero to image

--- components/sections/hero/Hero.tsx
+++ components/sections/hero/Hero.tsx
@@ -1,16 +1,13 @@
-import Image from "next/image";
-import hero from "@/public/hero/hero.webp";
 import styles from "./Hero.module.css";
+import React from "react";
 
 export default function Hero() {
     return (
         <section className={styles.section}>
-            <Image
-                src={hero}
-                fill
-                className={styles.image}
-                alt="Vardags rum ifrån ett av lewas projekt - glansbaggen"
-            />
+            <video autoPlay muted loop playsInline preload="auto">
+                <source src="/hero/hero_compressed.webm" type="video/webm" />
+                <source src="/hero/hero_compressed.mp4" type="video/mp4" />
+            </video>
         </section>
     );
 }
